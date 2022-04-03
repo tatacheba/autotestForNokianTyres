981c4a96dd621be753d4ba98d4c6883bf4087840
TatianaCheb <tanya.cheba12@gmail.com>
2022-04-03 20:36:53 +0300

    +

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,41 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Configuration.browserSize;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+
+    @BeforeAll
+    static void openBrowser() {
+        baseUrl = "https://www.nokiantyres.ru/";
+        browserSize = "1920x1080";
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+//
+//        String user = System.getProperty("user");
+//        String password = System.getProperty("password");
+//        Configuration.remote = "https://" + user + ":" + password + "@" + System.getProperty("remoteBrowser");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+    }
+
+    @AfterEach
+    void addAttachments() {
+        AttachAllure.screenshotAs("Last screenshot");
+        AttachAllure.pageSource();
+        AttachAllure.browserConsoleLogs();
+        AttachAllure.addVideo();
+        closeWebDriver();
+    }
+}
