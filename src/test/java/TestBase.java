1695f6c025a5e9374fcdaabff1ed0afbd3694cc1
TatianaCheb <tanya.cheba12@gmail.com>
2022-04-03 22:14:09 +0300

    Update TestBase.java

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -8,26 +8,38 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Configuration.browserSize;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.sleep;
 
 public class TestBase {
 
     @BeforeAll
     static void openBrowser() {
         baseUrl = "https://www.nokiantyres.ru/";
-        browserSize = "1920x1080";
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-//
-//        String user = System.getProperty("user");
-//        String password = System.getProperty("password");
-//        Configuration.remote = "https://" + user + ":" + password + "@" + System.getProperty("remoteBrowser");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
 
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "91");
+        String size = System.getProperty("size", "1920x1080");
+        String remoteUrl = System.getProperty("remoteUrl", "selenoid.autotests.cloud/wd/hub");
+        String login = System.getProperty("login", "user1");
+        String password = System.getProperty("password", "1234");
+
+        String url = "https://" + login + ":" + password + "@" + remoteUrl;
+        Configuration.remote = url;
+        Configuration.browser = browser;
+        Configuration.browserVersion = version;
+        Configuration.browserSize = size;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        AttachAllure.attachAsText("Browser: ", browser);
+        AttachAllure.attachAsText("Version: ", version);
+        AttachAllure.attachAsText("Remote Url: ", remoteUrl);
     }
 
     @AfterEach
@@ -36,6 +48,7 @@ public class TestBase {
         AttachAllure.pageSource();
         AttachAllure.browserConsoleLogs();
         AttachAllure.addVideo();
+        sleep(5000);
         closeWebDriver();
     }
 }
