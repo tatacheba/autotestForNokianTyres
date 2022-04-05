63984d31dfea845787bb6a509ebfaeeebd2769ff
TatianaCheb <tanya.cheba12@gmail.com>
2022-04-05 21:20:52 +0300

    Update TestBase.java

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -22,10 +22,10 @@ public class TestBase {
 
 //        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
-        String browser = System.getProperty("browser", "chrome");
-        String version = System.getProperty("version", "91");
-        String size = System.getProperty("size", "1920x1080");
-        String remoteUrl = System.getProperty("remoteUrl", "selenoid.autotests.cloud/wd/hub");
+//        String browser = System.getProperty("browser", "chrome");
+//        String version = System.getProperty("version", "91");
+//        String size = System.getProperty("size", "1920x1080");
+//        String remoteUrl = System.getProperty("remoteUrl", "selenoid.autotests.cloud/wd/hub");
         String user = System.getProperty("user");
         String password = System.getProperty("password");
         Configuration.remote = "https://" + user + ":" + password + "@" + System.getProperty("remoteUrl");
