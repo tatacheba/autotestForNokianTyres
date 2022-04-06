78e6258683739c4bb4c46d7ef93c5882986522fe
TatianaCheb <tanya.cheba12@gmail.com>
2022-04-06 10:30:14 +0300

    Update TestBase.java

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.sleep;
 
@@ -13,24 +12,24 @@ public class TestBase {
 
     @BeforeAll
     static void openBrowser() {
-        baseUrl = "https://www.nokiantyres.ru/";
+        Configuration.baseUrl = "https://www.nokiantyres.ru/";
+        Configuration.browserSize = System.getProperty("size", "1920x1080");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.pageLoadTimeout = 80000;
+        Configuration.browserVersion = System.getProperty("version");
+        SelenideLogger.addListener("allure", new AllureSelenide());
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+        //password and user for remote browser
+        String user = System.getProperty("user","user1");
+        String password = System.getProperty("password","1234");
+
+       DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://" + user + ":" + password + "@" + System.getProperty("remoteBrowser","selenoid.autotests.cloud/wd/hub");
 
 //        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
-//        String browser = System.getProperty("browser", "chrome");
-//        String version = System.getProperty("version", "91");
-//        String size = System.getProperty("size", "1920x1080");
-//        String remoteUrl = System.getProperty("remoteUrl", "selenoid.autotests.cloud/wd/hub");
-        String user = System.getProperty("user");
-        String password = System.getProperty("password");
-        Configuration.remote = "https://" + user + ":" + password + "@" + System.getProperty("url");
-
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
     }
 
