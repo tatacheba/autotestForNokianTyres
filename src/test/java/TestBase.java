66c673ca4b8a48bde584409b7c420ebd23978922
TatianaCheb <tanya.cheba12@gmail.com>
2022-04-06 10:55:51 +0300

    Update TestBase.java

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -13,23 +13,17 @@ public class TestBase {
     @BeforeAll
     static void openBrowser() {
         Configuration.baseUrl = "https://www.nokiantyres.ru/";
-//        Configuration.browserSize = System.getProperty("size", "1920x1080");
-//        Configuration.browser = System.getProperty("browser", "chrome");
-//        Configuration.pageLoadTimeout = 80000;
-//        Configuration.browserVersion = System.getProperty("version");
         SelenideLogger.addListener("allure", new AllureSelenide());
 
         //password and user for remote browser
-        String user = System.getProperty("user","user1");
-        String password = System.getProperty("password","1234");
+        String user = System.getProperty("user");
+        String password = System.getProperty("password");
 
        DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
         Configuration.remote = "https://" + user + ":" + password + "@" + System.getProperty("remoteBrowser","selenoid.autotests.cloud/wd/hub");
-
-//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
     }
 
